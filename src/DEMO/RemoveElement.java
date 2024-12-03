8935c40f758eafa1e67c85ff7afa1d585833dbcc
12002iya <115150152+12002iya@users.noreply.github.com>
2024-12-03 23:09:43 +0530

    Update RemoveElement.java

--- src/DEMO/RemoveElement.java
+++ src/DEMO/RemoveElement.java
@@ -1,27 +1,21 @@
 package DEMO;
 
 public class RemoveElement {
-
-
     public static void main(String[] args) {
-
-
-        int[] num = {3, 3, 3, 3, 2, 2};
+        int[] nums = {3, 3, 3, 3, 2, 2};
         int val = 3;
-        int[] ans = new int[num.length];
-        int count = 0;
-        for (int i = 0; i < num.length; i++) {
-
-            if (num[i] == 3) {
-                ans[i] = '-';
-
+    
+        int k = 0; 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) { 
+                nums[k] = nums[i]; // Move valid element to the front
+                k++;
             }
-            count++;
         }
-        for (int an : ans) {
-            System.out.println("number is "+an);
+        System.out.println("Number of elements not equal to " + val + " is: " + k);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
         }
-
-
     }
 }
