eebe628031b6169fb9a0200d228049ebb0b9688e
12002iya <115150152+12002iya@users.noreply.github.com>
2024-12-10 22:52:12 +0530

    Update BinarySearch.java
    Add the comments in the Code

--- src/DEMO/BinarySearch.java
+++ src/DEMO/BinarySearch.java
@@ -21,6 +21,7 @@ public class BinarySearch {
         int left=0;
         int right =arr.length-1;
         while (left<=right){
+            //find the mid element
             int mid=left+(right-left)/2;
 
             if (arr[mid]==target){
